3b24facb6fa4dcb1b1a53dde7c014f0d5e8f2b3f
BEU <evbolohov@yandex.ru>
2021-12-03 17:07:09 +0300

    init1

--- Занятие1/Домашние_задания/task1_1/main.py
+++ Занятие1/Домашние_задания/task1_1/main.py
@@ -1,3 +1,10 @@
+def enumer(list_: list):
+
+    for number, char in list(zip(numbers, chars)):  # TODO поэлементно объединить numbers и chars
+        print(f"{number}-{char}")
+    print(list(enumerate(chars)))
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    chars = ["a", "b", "c", "d", "e"]
+    task(chars)
